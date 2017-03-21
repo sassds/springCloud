fb81c7f971a0e1300366ac118d50f0326e131980
chujingbin <chujingbin@gmail.com>
2017-03-21 15:19:52 +0800

    feign

---
+++ feign/src/main/java/com/sassds/FeignApplication.java
@@ -0,0 +1,18 @@
+package com.sassds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+//用于启动服务发现功能
+@EnableDiscoveryClient
+//用于启动Fegin功能
+@EnableFeignClients
+@SpringBootApplication
+public class FeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignApplication.class, args);
+	}
+}
